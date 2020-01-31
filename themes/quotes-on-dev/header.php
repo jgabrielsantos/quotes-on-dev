88b9a79c5747cd1c772940f13c66d149ba160642
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-01-31 14:41:07 -0800

    added font link, changed message on 404 error

--- themes/quotes-on-dev/header.php
+++ themes/quotes-on-dev/header.php
@@ -12,6 +12,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link href="https://fonts.googleapis.com/css?family=Exo&display=swap" rel="stylesheet">
 		<script src="https://kit.fontawesome.com/4a78412303.js" crossorigin="anonymous"></script>
 
 	<?php wp_head(); ?>
