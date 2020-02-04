6b7c148c0f538eab597b6fc86a2ad51b3371e714
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-02-04 00:10:24 -0800

    added and styled archive page

---
+++ themes/quotes-on-dev/page-archives.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @package QOD_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="archives">
+				<header class="entry-header">
+					<?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+				</header><!-- .entry-header -->
+
+				<section class="authors">
+					<h2>Quote Authors</h2>
+					
+					<div class="items">
+						<?php $posts = get_posts('posts_per_page=-1');
+							foreach ($posts as $post) : setup_postdata($post); ?>
+							
+								<p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+						<?php endforeach;
+						wp_reset_postdata(); ?>
+					</div>
+				</section>
+
+				<section class="categories">
+					<h2>Categories</h2>
+					<div class="items">
+						<?php wp_list_categories('title_li=', array(
+							
+						)); ?>
+					</div>
+				</section>
+
+				<section class="tags">
+					<h2>Tags</h2>
+
+					<div class="items">
+						<?php wp_tag_cloud(array(
+							'smallest' => 1.25,
+							'largest'  => 1.25,
+							'unit'	   => 'rem'
+						)); ?>
+					</div>
+				</section>
+			</div>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
