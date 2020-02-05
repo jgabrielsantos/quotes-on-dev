258dc7d2ca85d168cf57249ee928d04958066cb5
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-02-04 18:44:08 -0800

    added loop to check if user is logged in on submit page, added tag.php for tags page

--- themes/quotes-on-dev/page-submit.php
+++ themes/quotes-on-dev/page-submit.php
@@ -11,9 +11,13 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
+				<?php if (is_user_logged_in()) {
+					get_template_part( 'template-parts/content', 'page' );
+				} else {
+					echo '<h2>Sorry, you must be loggedin to submit a quote!</h2>
+					<a href="http://localhost:8888/QUOTES-ON-DEV/wp-login.php?loggedout=true&wp_lang=en_CA">Click here to login.';
+				}
+				 ?>
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
